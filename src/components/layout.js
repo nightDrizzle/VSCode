66dd380594805cbcb402b4681343dcad8ce75fa6
nightDrizzle <1563607949@qq.com>
2021-10-14 22:31:53 +0800

    reset project structure

--- src/components/layout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Example = () => {
-  return <div>layout</div>;
-};
-
-Example.propTypes = {};
-
-export default Example;
--- src/test/main.js
+++
@@ -1,2 +0,0 @@
-const text = "main";
-console.log(text);
